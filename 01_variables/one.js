5c98a0bb563d0ff37bd6a054659469a65b0820a0
Krishna Pal <126250689+Krishna33pal@users.noreply.github.com>
2024-07-21 17:10:59 +0000

    VariablePractice...

---
+++ 01_variables/one.js
@@ -0,0 +1,21 @@
+const accountId = 14443;
+let accountEmail = "abc@gmail.com";
+var accountPass =  "12345";
+accountCity = "Satna";
+
+//accountId = 2  // not change it
+ 
+accountEmail = "ram@gmail.com"
+accountPass  = "55643";
+accountCity = "Chandigarh"
+
+// prefer not used var and 
+// because it used block and function scope!!!
+/*
+   var ka use na  kare tabhi sahi because the reason behind it
+     because of issue   in block scope and functional scope..... 
+*/
+
+console.table([accountEmail,accountId,accountPass,accountCity]);
+
+console.log(accountId);
