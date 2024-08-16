185bd5647896a9a667af1d246d3ca1b3f68ae940
Krishna Pal <126250689+Krishna33pal@users.noreply.github.com>
2024-08-16 16:35:47 +0000

    Operation in js

---
+++ DemoOperation/Operation.js
@@ -0,0 +1,24 @@
+
+//***************Operation ********************/
+// we can print power of 2  which is 5
+// console.log(2**5);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1+ 2+ "2");
+
+console.log(true);
+
+console.log(+true);
+console.log(+"");
+
+
+ let gameCounter = 100
+  //gameCounter++;
+  ++gameCounter;
+  console.log(gameCounter);
+  
+
+
+
